940dad01a11d796ec4fa55e1fefe91e145416781
Nathaniel <nathanielhiggins14@gmail.com>
2019-11-01 18:51:17 -0400

    everything but Javascript
    css is pretty much complete except for adjustments after js works for nav bar

--- js/index.js
+++ js/index.js
@@ -1,5 +1,14 @@
 $(document).on('click','.navShop',function() {
     $('navShop').removeClass('navShopSubINActive '),
     $('navShop').addClass('navShopSubActive ')
+    console.log('asdf')
 }
 );
+
+
+$( 'navShopSubINActive').mouseenter(function() {
+    $('navShopSubINActive').css("display", "flex"),
+    console.log('butt');
+    
+});
+
