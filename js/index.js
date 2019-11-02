46e31e9b36e8a302811078de99ad7a195065b99a
Nathaniel <nathanielhiggins14@gmail.com>
2019-11-01 21:32:53 -0400

    Final For Class
    still no Javascript but added custom made section and more perfected css.

--- js/index.js
+++ js/index.js
@@ -1,14 +1,24 @@
-$(document).on('click','.navShop',function() {
-    $('navShop').removeClass('navShopSubINActive '),
-    $('navShop').addClass('navShopSubActive ')
-    console.log('asdf')
-}
-);
+// $(document).on('click','.navShop',function() {
+//     $('navShop').removeClass('navShopSubINActive '),
+//     $('navShop').addClass('navShopSubActive ')
+//     console.log('asdf')
+// }
+// );
 
 
-$( 'navShopSubINActive').mouseenter(function() {
-    $('navShopSubINActive').css("display", "flex"),
-    console.log('butt');
+// $( 'navShopSubINActive').mouseenter(function() {
+//     $('navShopSubINActive').css("display", "flex"),
+//     console.log('butt');
     
-});
+// });
 
+
+
+$(document).ready(function () {
+    $('.navShopSubINActive').click(function () {
+        $(this).addClass('navShopSubActive');
+    }, function () {
+        $(this).removeClass('navShopSubINActive');
+        console.log('butthole')
+    });
+});
